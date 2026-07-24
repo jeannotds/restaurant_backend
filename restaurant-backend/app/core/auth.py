01409bad8ceb102b0685596803d360b8765f2d7e
Jeannot Diambu <diambujeannot@gmail.com>
2026-07-24 15:05:01 +0100

    clean code

--- restaurant-backend/app/core/auth.py
+++
@@ -1,8 +0,0 @@
-# from fastapi.security import OAuth2PasswordBearer
-# from fastapi import Depends, HTTPException, status
-# from sqlalchemy.orm import Session
-
-# from sqlalchemy.orm import get_db
-# from app.models.user import User
-# from app.core.jwt import decode_token
-
